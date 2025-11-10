57cd918cef8105fb7e1842f4d1ab6fe0c725b673
Barry Sande <barrysandebbs@gmail.com>
2025-11-10 21:46:37 +0300

    update: add password cleanup command to scheduler

--- start/scheduler.ts
+++ start/scheduler.ts
@@ -1,9 +1,11 @@
 import scheduler from 'adonisjs-scheduler/services/main'
 import ProcessJobs from '../commands/process_jobs.js'
+import CleanupPasswordTokens from '../commands/cleanup_password_tokens.js'
 import db from '@adonisjs/lucid/services/db'
 import { DateTime } from 'luxon'
 
 scheduler.command(ProcessJobs).everyFiveSeconds().withoutOverlapping()
+scheduler.command(CleanupPasswordTokens).quarterly().withoutOverlapping()
 
 scheduler
   .call(async () => {
