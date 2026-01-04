750fb2b6ef76b7e4a566be60a4192122fb9d7129
Barry Sande <barrysandebbs@gmail.com>
2026-01-04 20:45:06 +0300

    feat: enhance visited field in updateGemValidator to handle undefined values safely.

--- app/validators/file_upload.ts
+++ app/validators/file_upload.ts
@@ -10,7 +10,10 @@ export const updateGemValidator = vine.create({
   name: vine.string().trim().minLength(1).maxLength(255).optional(),
   location: vine.string().trim().minLength(1).maxLength(255).optional(),
   description: vine.string().trim().optional(),
-  visited: vine.boolean().optional(),
+  visited: vine
+    .boolean()
+    .optional()
+    .transform((value) => value ?? false), //https://vinejs.dev/docs/html_forms_and_surprises -> Checkboxes are not booleans. This covers for when a user has switched off JS in browser, in which case the adonis will receive undefined, which not a true/safe boolean in this case. With JS on Superforms sends a default false because of the bind:checked.
   rating: vine.number().min(1).max(5).optional(),
 })
 
